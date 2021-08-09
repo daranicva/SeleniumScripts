4740adb48c027ddf1b86b54506833b186f39b61d
daranicvagit config --global user.name daranicvacleargit clearcleargit config --global user.name daranicva@gmail.com <daranicva@gmail.com>
2021-08-09 12:56:57 +0530

    commiting Github to BranchX

---
+++ src/test/java/scripts/GitHub.java
@@ -0,0 +1,15 @@
+package scripts;
+
+public class GitHub {
+
+	public static void main(String[] args) {
+		String hungry ="yes";
+		if(hungry=="no")
+			System.out.println("eat");
+		else {
+			System.out.println("do wark...");
+		}
+
+	}
+
+}
